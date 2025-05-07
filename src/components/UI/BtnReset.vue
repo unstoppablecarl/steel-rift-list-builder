03778f236f5524958d037a97a7fd17fa2be54278
Carl Olsen <unstoppablecarlolsen@gmail.com>
2025-05-07 13:40:16 -0400

    armylist reset warning

--- src/components/UI/BtnReset.vue
+++ src/components/UI/BtnReset.vue
@@ -1,6 +1,22 @@
 <script setup>
 import {resetStores} from '../../store/helpers/store-save-load.js';
+import {BModal} from 'bootstrap-vue-next';
+import {ref} from 'vue';
+
+const modal = ref(false);
+
 </script>
 <template>
-  <BButton @click="resetStores()" size="sm" variant="danger">Reset</BButton>
+  <BButton @click="modal = !modal" v-b-modal.modal-center size="sm" variant="danger">Reset</BButton>
+  <BModal
+      v-model="modal"
+      centered
+      @ok="resetStores()"
+      ok-variant="danger"
+      title="Reset Army List?"
+  >
+    <div class="lead">
+      Are you sure you want to clear all army list data?
+    </div>
+  </BModal>
 </template>
